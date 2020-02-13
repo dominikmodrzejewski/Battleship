95ecf5d7cab023a103089200477d6cefd10fd247
Dominik Modrzejewski <dominikmodrzejewski99@gmail.com>
2020-02-13 16:20:45 +0100

    stage 1

---
+++ battleship.js
@@ -0,0 +1,12 @@
+// JavaScript source code
+
+let location1 = 3;
+let location2 = 4;
+let location3 = 5;
+
+let hits; //  iloœæ trafien
+let guess = 0; // komorka do sprawdzenia
+let guesses = 0; // ilosc prob
+
+let isSunk = false // sprawdza czy statek jest zatopiony
+
