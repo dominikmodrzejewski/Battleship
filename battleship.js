66a23a1fadaf4b3221cb3cd745cef952b4e7ef2d
Dominik Modrzejewski <dominikmodrzejewski99@gmail.com>
2020-02-13 22:25:38 +0100

    stage 2

--- battleship.js
+++ battleship.js
@@ -1,12 +1,40 @@
 // JavaScript source code
 
+"use strict";
+
 let location1 = 3;
 let location2 = 4;
 let location3 = 5;
 
-let hits; //  iloœæ trafien
-let guess = 0; // komorka do sprawdzenia
+let hits = 0; //  iloœæ trafien
+let guess; // komorka do sprawdzenia
 let guesses = 0; // ilosc prob
 
 let isSunk = false // sprawdza czy statek jest zatopiony
+while (isSunk == false) {
+    guess = prompt('Wybierz pole z zakresu 0-6?', '');
+    if (guess > 6 || guess < 0) {
+        alert("Podaj prawidlowe numer pola z zakresu 0-6")
+    }
+    else {
+        guesses++;
 
+        if (guess == location1 || guess == location2 || guess == location3) {
+            hits++;
+            alert("Trafiony!");
+
+
+
+            if (hits == 3) {
+                isSunk = true;
+                alert("Zatopiles moj okret");
+            }
+        }
+        else {
+            alert("Pud³o")
+        }
+    } 
+}
+let stats = "Potrzebowales " + guesses + " prob by zatopic okret, " +
+    "czyli twoja efektywnosc wynosi " + (guesses / 3) + ".";
+alert(stats);
